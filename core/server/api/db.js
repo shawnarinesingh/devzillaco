9d6e3f6389b75c542741e0a573b4a54903bcd621
Ubuntu <joshchandler88@gmail.com>
2015-08-15 21:02:21 +0000

    Browse roles and export db functions

---
+++ core/server/api/db.js
@@ -0,0 +1,46 @@
+// # DB API
+// API for DB operations
+var _                = require('lodash'),
+    Promise          = require('bluebird'),
+    dataExport       = require('../data/export'),
+    importer         = require('../data/importer'),
+    models           = require('../models'),
+    errors           = require('../errors'),
+    canThis          = require('../permissions').canThis,
+    utils            = require('./utils'),
+
+    api              = {},
+    db;
+
+api.settings         = require('./settings');
+
+/**
+ * ## DB API Methods
+ *
+ */
+db = {
+  /**
+   * ### Export Content
+   * Generate the JSON to export
+   * 
+   * @public
+   * @param {{context}} options
+   * @return {Promise} Export JSON format
+   */
+  exportContent: function (options) {
+    options = options || {};
+    
+    // Export data, otherwise send error 500
+    return canThis(options.context).exportContent.db().then(function () {
+      return dataExport().then(function (exportedData) {
+        return {db: [exportedData]};
+      }).catch(function (error) {
+        return Promise.reject(new errors.InternalServerError(error.message || error));
+      });
+    }, function () {
+      return Promise.reject(new errors.NoPermissionError('You do not have permission to export data (no rights).'));
+    });
+  }
+};
+
+module.exports = db;
