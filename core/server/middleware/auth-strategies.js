fe722d9340be1ca1ca8b710668349f1a3395cda9
Ubuntu <joshchandler88@gmail.com>
2015-08-15 18:55:33 +0000

    Authentication API

--- core/server/middleware/auth-strategies.js
+++ core/server/middleware/auth-strategies.js
@@ -2,3 +2,12 @@ var passport = require('passport'),
     BearerStrategy = require('passport-http-bearer').Strategy,
     ClientPasswordStrategy = require('passport-oath2-client-password').Strategy,
     models = require('../models');
+
+/**
+ * ClientPasswordStrategy
+ * 
+ * This strategy is used to authenticate registered OAuth clients. It is
+ * employed to protect the `token` endpoint, which consumers use to obtain
+ * access tokens.  The OAuth 2.0 specification suggests that clients use the
+ * HTTP Basic scheme to authenticate (not implemented yet).
+ */
