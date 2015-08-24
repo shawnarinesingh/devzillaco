4cc557bdef16407cce101d3ede8908620f7a1146
Ubuntu <joshchandler88@gmail.com>
2015-08-24 00:43:55 +0000

    Delete build directory

--- core/build/app.js
+++
@@ -1,9 +0,0 @@
-var React = require('react'),
-    App = require('./components/App');
-
-function run() {
-  var app = React.render(React.createElement(App, null), document.body);
-  
-  return app;
-}
-
--- core/build/server.js
+++
@@ -1,3 +0,0 @@
-var App = require('./components/App');
-
-module.exports = App;
